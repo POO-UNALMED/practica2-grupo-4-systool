29d2d2cb6d57a72aecc5ee2a3716b5959fd00284
SebasWolf214 <69688254+SebasWolf214@users.noreply.github.com>
2020-11-23 13:02:35 -0500

    New Ventana

--- src/VentanaInicio.java
+++ src/VentanaInicio.java
@@ -1,4 +1,52 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class VentanaInicio {
-
+public class VentanaInicio extends Application {
+	public void start(Stage primario) throws Exception{
+		BorderPane inicio = new BorderPane();
+		BorderPane izqui = new BorderPane();
+		BorderPane dere = new BorderPane();
+		
+		
+		
+		////////////////////////////////////////
+		izqui.setTop(new Label("BIENVENIDO A SYSTOOL"));
+		
+		
+		
+		inicio.setLeft(izqui);
+		
+		
+		////////////////////////////////
+		
+		
+		inicio.setRight(dere);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		primario.setTitle("SYSTOOL 2.0v");
+		Scene prisce = new Scene (inicio,400,400);
+		primario.setScene(prisce);
+		primario.show();
+		
+	}
+	
+	public static void main(String args[]) {
+		launch(args);
+	}
+	
+	
+	
 }
