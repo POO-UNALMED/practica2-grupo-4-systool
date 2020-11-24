aec89bfc88c107f94eac6a4de1e87d720e38f3a1
SebasWolf214 <69688254+SebasWolf214@users.noreply.github.com>
2020-11-23 20:42:22 -0500

    Nueva Escena arregada + modificaciones de scena 1

--- src/uiMain/VentanaInicio.java
+++ src/uiMain/VentanaInicio.java
@@ -1,11 +1,16 @@
 package uiMain;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,20 +20,48 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class VentanaInicio extends Application {
-	public void start(Stage primario) throws Exception{
+	public static void main(String args[]) {
+		launch(args);
+	}
+	
+	Stage primario;
+	Scene prisce;
+	Scene user;
+	BorderPane ustage;
+	GridPane p5;
+	Label hoja1;
+	TextArea hoja;
+	public void start(Stage primaryStage) throws Exception{
+		primario = primaryStage;
+
+		//ESCENARIO USER
+		BorderPane ustage = new BorderPane();
+		Scene user = new Scene(ustage,650,420);
+		MenuBar mainmenu = new MenuBar();
+		Menu profe = new Menu("Profesor");
+		Menu grado = new Menu("Grado");
+		Menu studen = new Menu("Estudiante");
+		mainmenu.getMenus().addAll(profe, grado, studen);
+		ustage.setTop(mainmenu);
+		////////////////////////////////////////////
+		
+		
+		
+		
+		//ESCENARIO DEFAULT
 		BorderPane inicio = new BorderPane();
 		inicio.setPadding(new Insets(15,15,15,15));
-		
-		BorderPane p1 = new BorderPane();
-		BorderPane p2 = new BorderPane();
-		
+		BorderPane p1 = new BorderPane(); //PANEL IZQUIERDO
+		BorderPane p2 = new BorderPane(); //PANEL DERECHO
+		//PANELES IZQUIERDOS
 		GridPane p3 = new GridPane();
 		BorderPane p4 = new BorderPane();
+		//PANELES DERECHOS
 		GridPane p5 = new GridPane();
 		GridPane p6 = new GridPane();
-		
 		inicio.setLeft(p1);
 		inicio.setRight(p2);
+		//DEFAULT DE LOS PANELES
 		////////////////////////////////////////
 		p1.setTop(p3);
 		Label bienvenida = new Label("BIENVENIDO A SYSTOOL");
@@ -40,50 +73,77 @@ public class VentanaInicio extends Application {
 		////////////////////////////////
 		p1.setCenter(p4);
 		Button ingre = new Button("Ingresar");
+		
+		//HANDLER DEL BOTON
+		ingre.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				primario.setScene(user);
+				primario.setTitle("SYSTOOL: Encantado de ayudar");
+			}
+				
+		});
+		
+		
 		Image imaini = new Image(getClass().getResourceAsStream("/imagenes/sparta.jpg"));
 		ImageView im01 = new ImageView(imaini);
-		Label imalabel = new Label("SYSTOOL", new ImageView(imaini));
+		im01.setFitHeight(200);
+		im01.setFitWidth(200);
 		p4.setBottom(ingre);
-		p4.setTop(imalabel);
+		p4.setTop(im01);
 		p4.setPadding(new Insets(15,15,15,15));
-		p4.setAlignment(ingre, Pos.CENTER);
-		p4.setAlignment(imalabel, Pos.CENTER);
+		BorderPane.setAlignment(ingre, Pos.CENTER);
+		BorderPane.setAlignment(im01, Pos.CENTER);
 		
 		///////////////////////////////////////////
 		p2.setTop(p5);
-		Label hoja1 = new Label("Pues me gustan los monitos chinos y pues tambien soy JOvenciTO,\r\ntengo 20 y me gusta la pepsi dos mas dos es cuatro y me quiero morir");
-		
+		Label hoja1 = new Label("Sebastian Agudelo Osorio,\r\n20 años, me gusta la pepsi, la animación y la musica");
+		Label hoja2 = new Label("Daniela Guardia Cuervo,\r\n20 años, me gusta la pepsi, la animación y la musica");
+		Label hoja3 = new Label("Cristian David Quinchia Ramirez,\r\n20 años, me gusta la pepsi, la animación y la musica");
 		hoja1.setTextFill(Color.web("#000000"));
-		hoja1.setPrefWidth(380);
-		hoja1.setPrefHeight(10);
+		hoja1.setPrefWidth(50);
+		hoja1.setPrefHeight(50);
 		p5.setPadding(new Insets(15,15,15,15));
 		p5.setAlignment(Pos.CENTER);
 		p5.add(hoja1,1,1);
-		
+		p5.setStyle("-fx-border-color: black");
 		
 		//////////////////////////////////
 		p2.setCenter(p6);
+		Image fot01 = new Image(getClass().getResourceAsStream("/fotos/sebas.png"));
+		ImageView prog = new ImageView(fot01);
+		prog.setFitHeight(200);
+		prog.setFitWidth(150);
+		p6.add(prog,1,1);
+		p6.setPadding(new Insets(15,15,15,15));
+		p6.setAlignment(Pos.CENTER);
+		p6.setStyle("-fx-border-color: black");
+		//FIN DEL DEFAULT
+		///////////////////////////////////////////
 		
+		///////////////////////////////////
 		
-		
-		
-		
-		
-		
-		
-		
-		
+		//ESCENARIO INCICIAL
 		primario.setTitle("SYSTOOL 2.0v");
-		Scene prisce = new Scene (inicio,600,380);
+		Scene prisce = new Scene (inicio,650,420);
 		primario.setScene(prisce);
 		primario.show();
 		
+		/////////////////////////////////
+		
+		
+		
+		
+		
+		
+		
 	}
 	
-	public static void main(String args[]) {
-		launch(args);
-	}
 	
 	
-	
+
 }
+	
+	
+	
+
